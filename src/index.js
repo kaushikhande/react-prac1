ed587cc4a00a726bbbbcc76d5af77106874721a8
kaushikhande <kaushikhande302@gmail.com>
2020-05-30 19:18:45 +0530

    Tick tick tick

--- src/index.js
+++ src/index.js
@@ -22,10 +22,25 @@ const element = (
   </h1>
 );
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+function tick() {
+  const element1 = (
+    <div>
+      <h1>Hello, World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element1,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
+
+// ReactDOM.render(
+//   element1,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
