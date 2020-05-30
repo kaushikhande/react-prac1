72674cb6ef8d2d9835d733754b9961ef2d3bf7ee
kaushikhande <kaushikhande302@gmail.com>
2020-05-30 18:43:11 +0530

    Pass username to the element.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const name = 'Mike Perez';
-const element = <h1>Hello, {name}</h1>;
+// const element = <h1>Hello, {name}</h1>;
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Mike',
+  lastName: 'Perez',
+}
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
   element,
