95ff82be3c8d64a18f47cc4e6feda592f42e9157
kaushikhande <kaushikhande302@gmail.com>
2020-06-01 07:34:54 +0530

    Render app1 component with Welcome component.

--- src/index.js
+++ src/index.js
@@ -16,31 +16,46 @@ const user = {
   lastName: 'Perez',
 }
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
-function tick() {
-  const element1 = (
+function App1() {
+  return (
     <div>
-      <h1>Hello, World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <Welcome name="Sara" />
+      <Welcome name="Cahel" />
+      <Welcome name="Edite" />
     </div>
-  );
-  ReactDOM.render(
-    element1,
-    document.getElementById('root')
   );
 }
 
-setInterval(tick, 1000);
-
-// ReactDOM.render(
-//   element1,
-//   document.getElementById('root')
+const element = <Welcome name="Sara" />;
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
 // );
+
+// function tick() {
+//   const element1 = (
+//     <div>
+//       <h1>Hello, World</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element1,
+//     document.getElementById('root')
+//   );
+// }
+
+// setInterval(tick, 1000);
+
+ReactDOM.render(
+  <App1 />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
